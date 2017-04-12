5b267a497372afcf918eecb0805a9e9124204043
Arnaud <nonomotte@gmail.com>
2017-04-12 23:23:51 +0200

    3eme realase

--- arnaud/src/main/java/arnaud/App.java
+++ arnaud/src/main/java/arnaud/App.java
@@ -1,8 +1,17 @@
 package arnaud;
 
 import java.io.FileReader;
+import java.io.FileWriter;
+
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.collections.CollectionUtils;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -20,22 +29,36 @@ public class App
         try
         {
 	        CSVReader reader = new CSVReader(new FileReader("data.csv"));
-	        try
-			{
-	        	List<String[]> myEntries = reader.readAll();
-	        	for (String[] item : myEntries) 
-		        {
-		    		for (String i : item) 
-		    		{
-		    			nb++;
-		    			
-		    			monmax = var.max(Integer.parseInt(i), monmax);
-		    		}
+    		CSVWriter writer;
+			try {
+				writer = new CSVWriter(new FileWriter("data-filtered.csv"));
+		        try
+				{
+		        	List<String[]> myEntries = reader.readAll();
+		        	for (String[] item : myEntries) 
+			        {
+		        		List<String> list = Arrays.asList(item);
+		        		Vector<String> out = new Vector<String>();
+		        		CollectionUtils.select(list, new MonPredicat<String>(), out);
+		        		System.out.println("OUT :" + out);
+			    		for (String i : out) 
+			    		{
+			    			nb++;
+			    			
+			    			monmax = var.max(Integer.parseInt(i), monmax);
+			    		}
+			    		String[] ret = out.toArray(new String[0]);
+			    		writer.writeNext(ret);
+					}
 				}
-			}
-			catch(IOException e)
-			{
-				System.out.println("Problème de lecture");
+				catch(IOException e)
+				{
+					System.out.println("Problème de lecture");
+				}
+	    		writer.close();
+			} 
+			catch (IOException e1) {
+				e1.printStackTrace();
 			}
 		}
 		catch(FileNotFoundException e)
