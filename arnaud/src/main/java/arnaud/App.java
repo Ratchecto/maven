616b368de99c41a0da1fc2e42a7549c2e2869e0e
Arnaud <nonomotte@gmail.com>
2017-04-12 23:12:29 +0200

    Seconde release

--- arnaud/src/main/java/arnaud/App.java
+++ arnaud/src/main/java/arnaud/App.java
@@ -1,22 +1,51 @@
 package arnaud;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class App 
 {
-    public static int max(int a, int b)
-    {
-    	//return a > b ? a : b ;
-    	
-    	//Le bon stagiaire :
-    	return a;
-    }
+
 	
     public static void main( String[] args )
     {
-    	int result = max(4, 5);
-        System.out.println(result);
+    	/*int result = max(4, 5);
+        System.out.println(result);*/
+    	
+    	int monmax = 0, nb = 0;
+    	obj var = new obj();
+        try
+        {
+	        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+	        try
+			{
+	        	List<String[]> myEntries = reader.readAll();
+	        	for (String[] item : myEntries) 
+		        {
+		    		for (String i : item) 
+		    		{
+		    			nb++;
+		    			
+		    			monmax = var.max(Integer.parseInt(i), monmax);
+		    		}
+				}
+			}
+			catch(IOException e)
+			{
+				System.out.println("Problème de lecture");
+			}
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("Fichier non trouvé");
+		}
+		
+		System.out.println("Nombre lu : " + nb);
+		System.out.println("Maximum : " + monmax);
     }
+   
+
 }
--- arnaud/src/test/java/arnaud/AppTest.java
+++ arnaud/src/test/java/arnaud/AppTest.java
@@ -33,8 +33,8 @@ public class AppTest
      */
     public void testApp()
     {
-        App var = new App();
+        obj var = new obj();
         assertEquals(5, var.max(4, 5));
-        assertEquals(5, var.max(4, 5));
+        assertEquals(5, var.max(5, 4));
     }
 }
